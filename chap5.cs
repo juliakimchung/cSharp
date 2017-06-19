854051e60367912e89000e0763a86f30f34483ca
juliakimchung <julhkm@gmail.com>
2017-06-19 13:43:41 -0500

    namespace

---
+++ chap5.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Chapter5App
+{
+	class Chapter5
+	{
+		static void Main(string[] args)
+		{
+			var tc = new testnamespaceclass();
+			tc.Test();
+		}
+	}
+}
---
+++ namespacetest.cs
@@ -0,0 +1,12 @@
+using System;
+namespace testnamespace
+{
+	public class testnamespaceclass
+	{
+		public void Test()
+		{
+			Console.WriteLine("This is a test for using other namespaces");
+		}
+	}
+
+}
